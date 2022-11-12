fc82b2feac1b27e5fe42270510e7bb02a1871988
Your Name <youremail@email.com>
2022-11-12 10:26:48 -0500

    Accesses to the Lambdas

--- readterra.js
+++ readterra.js
@@ -30,6 +30,11 @@ exports.handler = async (event) => {
         
 			var response = {
 				'statusCode': 200,
+                                headers: {
+                                  "Access-Control-Allow-Headers" : "Content-Type",
+                                  "Access-Control-Allow-Origin": "*",
+                                  "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+                                },
 				'body': JSON.stringify({
 					message: data
 				})
