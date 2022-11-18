320ed207c527d14f42fa158c53dce127ed8de95b
Your Name <youremail@email.com>
2022-11-18 16:10:06 -0500

    Main 9

--- main.tf
+++ main.tf
@@ -108,21 +108,6 @@ resource "aws_api_gateway_rest_api" "apiLambda" {
   description   = "An API for demonstrating CORS-enabled methods."
 }
 
-module "cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-
-  api_id          = aws_api_gateway_rest_api.apiLambda.id
-  api_resource_id = aws_api_gateway_resource.writeResource.id
-}
-
-module "cors2" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
-
-  api_id          = aws_api_gateway_rest_api.apiLambda.id
-  api_resource_id = aws_api_gateway_resource.readResource.id
-}
 
 resource "aws_api_gateway_resource" "writeResource" {
   rest_api_id = aws_api_gateway_rest_api.apiLambda.id
